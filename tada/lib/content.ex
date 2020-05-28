19080a7969d362633dc8539e1714bd6adb49e61b
Bruce Tate <bruce@rapidred.com>
2020-05-28 14:59:24 -0400

    content

---
+++ tada/lib/content.ex
@@ -0,0 +1,50 @@
+defmodule Tada.Content do
+  def passage(:bruce) do
+    """
+    Tomorrow, tomorrow, and tomorrow
+    Creeps in this petty pace from day to day,
+    To the last sylable of recorded time.
+    And all our yesterdays have lighted fools, 
+    the way to dusty death. Out, out, brief candle!
+    
+    Life is but a walking shadow, a poor player 
+    that struts and frets his way upon the stage, 
+    and then is heard no more. 
+    It is a tale told by an idiot
+    Full of sound and fury, 
+    signifying nothing. 
+    """
+  end
+  
+  def passage(:matt) do
+    """
+    Well, my lord,
+    Since you have promised not to take my life,
+    I will, without reserve, declare the truth.
+
+    He draws the arrow from his belt, and fixes his eyes
+    sternly upon the governor.
+
+    If that my hand had struck my darling child,
+    This second arrow I had aimed at you,
+    And, be assured, I should not then have missed.    
+    """
+  end
+  
+  def passage(:wanjiku) do
+    """
+    Hope is the thing with feathers
+    That perches in the soul,
+    And sings the tune without the words
+    And never stops at all,
+    And sweetest in the gale is heard;
+    And sore must be the storm
+    That could abash the little bird
+    That kept so many warm.
+    I’ve heard it in the chillest land,
+    And on the strangest sea;
+    Yet, never, in extremity,
+    It asked a crumb of me.
+    """
+  end
+end
