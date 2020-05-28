a6b5f76b54d3ca0caa693d6bc8a8155a604fed5a
Matt Willy <matt.willy@gmail.com>
2020-05-28 11:58:10 -0600

    created the eraser reducer

--- tada/test/passage_test.exs
+++ tada/test/passage_test.exs
@@ -1,14 +1,32 @@
-
 defmodule PassageTest do
   use ExUnit.Case
   alias Tada.Passage
 
   test "passage constructor" do
-    assert passage(:short).text_state == "I am happy" 
-    assert [[_,_,_,_,_], [_,_,_,_,_]] = passage(:short).chars_to_remove_per_step
+    assert passage(:short).text_state == "I am happy"
+    assert [[_, _, _, _, _], [_, _, _, _, _]] = passage(:short).plan
+  end
+
+  test "reduces intermediate step" do
+    passage = passage(:intermediate)
+    actual = Passage.erase(passage)
+
+    expected = %Passage{
+      text_state: "_2_45",
+      plan: []
+    }
+
+    assert actual == expected
   end
 
   def passage(:short) do
     Passage.new("I am happy", 2)
+  end
+
+  def passage(:intermediate) do
+    %Passage{
+      text_state: "12345",
+      plan: [[1, 3]]
+    }
   end
 end
