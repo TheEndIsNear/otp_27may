861813086142934929fcdc4a5316a94b9a6c7b57
Matt Willy <matt.willy@gmail.com>
2020-05-29 19:15:29 -0600

    run the ironman suit up to add more protection to the code base

--- mix.exs
+++ mix.exs
@@ -3,6 +3,18 @@ defmodule Tada.MixProject do
 
   def project do
     [
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ],
+      dialyzer: [
+        ignore_warnings: ".dialyzer_ignore.exs",
+        list_unused_filters: true,
+        plt_file: {:no_warn, "tada.plt"}
+      ],
       app: :tada,
       version: "0.1.0",
       elixir: "~> 1.10",
@@ -22,6 +34,12 @@ defmodule Tada.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:git_hooks, "~> 0.4.2", only: :dev, runtime: false},
+      {:excoveralls, "~> 0.13.0", only: :test},
+      {:credo, "~> 1.4.0", only: :dev, runtime: false},
+      {:mix_test_watch, "~> 1.0.2", only: :dev, runtime: false},
+      {:dialyxir, "~> 1.0.0", only: :dev, runtime: false},
+      {:ex_doc, "~> 0.22.1", only: :dev, runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
